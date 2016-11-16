4ac67f531b15058728d39be4f5d2174cc9c016e3
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-11-16 09:48:15 -0500

    Use styles for selected nodes

--- js/index.js
+++ js/index.js
@@ -8,15 +8,16 @@ var bgRect = d3.select('svg').append('rect')
 var g = d3.select('svg')
           .append('g')
 
+var gBlocks = g.append('g');
+var gRoads = g.append('g')
+var gLegend = g.append('g')
 
 
 var projection = d3.geoMercator()
 .scale(1000000)
-.translate([-1240829, 817805])
 
 var path = d3.geoPath()
 .projection(projection);
-
 
 d3.json("block_trees.json", function(error, data) {
     if (error) throw error;
@@ -24,21 +25,10 @@ d3.json("block_trees.json", function(error, data) {
     var treeNames = d3.set(data.features.map(function(d) {
         return d.properties.most_common_tree_name}));
 
-
-    var treeDensities = data.features.map(function(d) {
-            return d.properties.total_tree_count / d.properties.area;
-        })
-    var opacityExtent = d3.extent(treeDensities)
-    // scale the opacity according to tree density
-    var opacityScale = d3.scaleLinear()
-        .domain(opacityExtent)
-        .range([0,1]);
-
     var colorScale = d3.scaleOrdinal()
     .domain(treeNames)
     .range(d3.schemeCategory20b.concat(d3.schemeCategory20c));
 
-    var gBlocks = g.append('g');
 
     function selectSameBlocks(d) {
         // select all blocks with the same most common tree
@@ -50,10 +40,6 @@ d3.json("block_trees.json", function(error, data) {
         return sameBlocks;
     }
 
-    bgRect.on('mouseover', function(d) {
-        gBlocks.selectAll('.block').style('opacity', 0.4);
-        gBlocks.selectAll('.block').attr('stroke-width', 0);
-    });
 
     gBlocks.selectAll('.block')
     .data(data.features)
@@ -63,15 +49,13 @@ d3.json("block_trees.json", function(error, data) {
     .attr('d', path)
     .attr('stroke', 'black')
     .style('fill', function(d) { return colorScale(d.properties.most_common_tree_name) })
-    .style('opacity', 0.4)
     .on('mouseover', function(d) {
-        gBlocks.selectAll('.block').style('opacity', 0.4);
-        gBlocks.selectAll('.block').attr('stroke-width', 0);
-        selectSameBlocks(d).style('opacity', 1.0)
-        selectSameBlocks(d).attr('stroke-width', 10);
-    })
-    ;
-    //.style('opacity', function(d) { return opacityScale(d.properties.total_tree_count / d.properties.area); });
+        gBlocks.selectAll('.block').classed('selected', false);
+        selectSameBlocks(d).classed('selected', true);
+    });
+
+    // mouse moves out of the map area
+    bgRect.on('mouseover', function(d) { gBlocks.selectAll('.block').classed('selected', false); });
 
     /* scale to fit all of cambridge */
     // https://bl.ocks.org/mbostock/4699541
@@ -84,35 +68,17 @@ d3.json("block_trees.json", function(error, data) {
     translate = [width / 2 - scale * x, height / 2 - scale * y];
 
     g.attr('transform', "translate(" + translate + ")scale(" + scale + ")")
-    d3.json("roads.topo", function(error, data1) {
-        console.log('data1:', data1);
-        
-        var gRoads = g.append('g')
-
-        gRoads.selectAll('.road')
-        .data(topojson.feature(data1,data1.objects.roads).features)
-        .enter()
-        .append('path')
-        .attr("class", "road")
-        .attr('d', path)
-        .style('stroke', 'black')
-        .style('stroke-width', '1px')
-        .style('fill', 'transparent')
-        .style('opacity', 0.6)
-    });
+                       
+    //add the legend
+    //
 });
 
-                           
-
-
-
-
-           
-
-
-
-
-           
-
-
-
+// add the roads
+d3.json("roads.topo", function(error, data1) {
+    gRoads.selectAll('.road')
+    .data(topojson.feature(data1,data1.objects.roads).features)
+    .enter()
+    .append('path')
+    .attr("class", "road")
+    .attr('d', path)
+});
